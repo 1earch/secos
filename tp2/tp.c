8a8b7db6224cbeb5e2819a37ba1476525d1afdff
Léarch <learch@firost.fr>
2019-01-11 19:39:16 +0100

    Question 3.4: bp_handler+bp_trigger

--- tp2/tp.c
+++ tp2/tp.c
@@ -1,8 +1,12 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <excp.h>
+#include <intr.h>
 
 extern info_t *info;
+
+extern int_desc_t IDT[IDT_NR_DESC];
 
 /* Enable interrupts. */
 void test_irq()
@@ -12,7 +16,31 @@ void test_irq()
 }
 
 
+/* #BP handler. */
+void bp_handler()
+{
+  debug("Breakpoint reached!\n");
+}
+
+/* #BP triggerer. */
+void bp_trigger()
+{
+  __asm__("int3");
+}
+
+
+/* Main function. */
 void tp()
 {
   //test_irq();
+
+  idt_reg_t idtr;
+  get_idtr(idtr);
+  debug("\nIDT loaded at: 0x%x\n", idtr.addr);
+
+  debug("\nBefore breakpoint...\n");
+  idtr.desc[BP_EXCP].offset_1 = (uint16_t) ((uint32_t)bp_handler & 0xFFFF);
+  idtr.desc[BP_EXCP].offset_2 = (uint16_t) ((uint32_t)bp_handler>>16 & 0xFFFF);
+  bp_trigger();
+  debug("After breakpoint...\n");
 }
