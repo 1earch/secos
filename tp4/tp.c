38699c44b876762ae30e7a2fa6e889ad0130d366
Léarch <learch@firost.fr>
2019-01-12 22:18:07 +0100

    Paging initialization

--- tp4/tp.c
+++ tp4/tp.c
@@ -7,19 +7,32 @@
 
 extern info_t *info;
 
-void tp()
+void show_cr3()
 {
   cr3_reg_t cr3 = { .raw = get_cr3() };
   debug("\nCR3 = %p\n", cr3.raw);
   debug("  - PWT  = %d\n", cr3.pwt);
   debug("  - PCD  = %d\n", cr3.pcd);
   debug("  - addr = %3p\n", cr3.addr);
+}
 
-  debug("\nAllocating a PDE entry at 0x600000...\n");
+
+void init_identity_mapping()
+{
+  // Allocate PGD and PGT
   pde32_t* pgd = (pde32_t*) 0x600000;
-  debug("  .. done!\n");
+  pte32_t* ptb = (pte32_t*) 0x601000;
 
-  debug("Updating cr3 with this address...\n");
+  // PGT identity-mapping
+  for (int i=0; i < 1024; i++)
+    pg_set_entry(&ptb[i], PG_RW | PG_KRN, i);
+
+  // PGD initialization
+  memset((void*)pgd, 0, PAGE_SIZE);
+  pg_set_entry(&pgd[0], PG_RW | PG_KRN, page_nr(ptb));
+
+  // Paging activation
+  debug("Updating cr3 with PGD address...\n");
   set_cr3((uint32_t) pgd);
   debug("  .. done!\n");
 
@@ -28,4 +41,10 @@ void tp()
   cr0.pg = 1;
   set_cr0(cr0);
   debug("  .. done!\n");
+}
+
+void tp()
+{
+  show_cr3();
+  init_identity_mapping();
 }
