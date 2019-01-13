8f9b501bacdd6e4a3913101ef267551052890852
Léarch <learch@firost.fr>
2019-01-13 11:15:22 +0100

    Mapping PTB1 (questions 5 and 6)

--- tp4/tp.c
+++ tp4/tp.c
@@ -19,17 +19,27 @@ void show_cr3()
 
 void init_identity_mapping()
 {
-  // Allocate PGD and PGT
-  pde32_t* pgd = (pde32_t*) 0x600000;
-  pte32_t* ptb = (pte32_t*) 0x601000;
+  uint32_t i;
 
-  // PGT identity-mapping
-  for (int i=0; i < 1024; i++)
-    pg_set_entry(&ptb[i], PG_RW | PG_KRN, i);
+  // Allocate PGD and PGT
+  pde32_t* pgd  = (pde32_t*) 0x600000;
+  pte32_t* ptb1 = (pte32_t*) 0x601000;
+  pte32_t* ptb2 = (pte32_t*) 0x602000;
+
+  // PTB1 identity-mapping
+  for (i=0; i < PTE32_PER_PT; i++)
+    pg_set_entry(&ptb1[i], PG_RW | PG_KRN, i);
+
+  // PTB2 mapping 0x601000:
+  //   in entry n°513 of PTB2, we must have mapped address 0x601 (1537)
+  //   so we must put in first entry of PTB2 1024 (ie 1537-513=1024)
+  for (i=0; i < PTE32_PER_PT; i++)
+    pg_set_entry(&ptb2[i], PG_RW | PG_KRN, i+1024);
 
   // PGD initialization
   memset((void*)pgd, 0, PAGE_SIZE);
-  pg_set_entry(&pgd[0], PG_RW | PG_KRN, page_nr(ptb));
+  pg_set_entry(&pgd[0], PG_RW | PG_KRN, page_nr(ptb1));
+  pg_set_entry(&pgd[1], PG_RW | PG_KRN, page_nr(ptb2));
 
   // Paging activation
   debug("Updating cr3 with PGD address...\n");
@@ -41,6 +51,10 @@ void init_identity_mapping()
   cr0.pg = 1;
   set_cr0(cr0);
   debug("  .. done!\n");
+
+  // Display a PTB entry
+  for (i=0; i < 10; i++)
+    debug("PTB[%d]: %p\n", i, ptb1[i].addr);
 }
 
 void tp()
